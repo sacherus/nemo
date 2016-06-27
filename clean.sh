b59f80e92be0d8c82bc99282fd0c42f365ece9fd
sacherus <sacherus@gmail.com>
2016-06-27 02:09:11 +0200

    Before proofreading

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm main.aux main.bbl main.bcf main.blg main.log main.pdf main.run.xml main.tdo main.toc texput.log
+rm -f main.aux main.bbl main.bcf main.blg main.log main.pdf main.run.xml main.tdo main.toc texput.log
